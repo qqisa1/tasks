be796a9384e404561ddc59d277e73b0906709103
qqisa1 <ikramsabirov96@gmail.com>
2023-02-07 18:56:19 +0600

    feat: 04-02

---
+++ 04-02/script.js
@@ -0,0 +1,19 @@
+let array = [1, 2, 3, 4, 5];
+let result = 0;
+for (let i = 0; i < array.length; i++) {
+  result += array[i];
+}
+console.log(result);
+
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for(let a = 0; a < arr.length; a++){
+    console.log(arr[a]**2);
+}
+
+for (let i = 2; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}

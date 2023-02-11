a9d9263df457066fbc365f2e437d047a90835679
qqisa1 <ikramsabirov96@gmail.com>
2023-02-11 19:13:06 +0600

    09-02

--- 09-02/script.js
+++ 09-02/script.js
@@ -5,19 +5,19 @@
    example([4, 1, 1, 1, 4]) => [8, 2, 2, 2, 8]
    example([2, 2, 2, 2, 2, 2]) => [4, 4, 4, 4, 4, 4]*/
 
-function DoubleValue() {
+/*function DoubleValue() {
   let numbers = [1, 2, 3, 4, 5];
   return console.log(numbers.map(numbers => numbers * 2));
  }
  let result = DoubleValue();
- console.log(result);
+ console.log(result);*/
 
 /*2) Напишите функцию, которая заполняет новый массив предоставленным значением
    и затем ее возвращает. Функция должна первым параметром принимать
    элемент (значение для массива), вторым параметром размер массива.
    Например: myFunc('a', 3)   // ['a', 'a', 'a']*/
 
- function fillArray(element, size) {
+/* function fillArray(element, size) {
    let newArray = [];
    for (let i = 0; i < size; i++) {
      newArray.push(element);
@@ -25,7 +25,7 @@ function DoubleValue() {
    return newArray;
  }
 
- console.log(fillArray('a', 3));
+ console.log(fillArray('a', 3));*/
 
 
  /*3) Напишите функцию, которая разворачивает массив в обратном порядке
@@ -34,13 +34,16 @@ function DoubleValue() {
    const data = [1, 2, 3];
    myFunc(data);  // [3, 2, 1];*/
 
- function revers(users = ['wer', 'ali', 'git']) {
-     let reversed = users.reverse();
-     console.log('reversed', reversed);
- }
 
- revers();
 
- function reverse () {
 
- }
+let arr = [ 1, 2, 3,];
+function reverse() {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        console.log(arr[i]);
+    }
+}
+
+reverse();
+
+

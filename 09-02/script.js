5a2fca2f4cb911107e4a8e8b5667fcac8e810570
qqisa1 <ikramsabirov96@gmail.com>
2023-02-11 18:38:43 +0600

    feat: 09-02

---
+++ 09-02/script.js
@@ -0,0 +1,42 @@
+/*1) На старте вы получаете массив. Необходимо написать функцию, которая будет
+   возвращать массив удвоенных значений исходного массива.
+   Пример кода:
+   example([1, 2, 3]) => [2, 4, 6]
+   example([4, 1, 1, 1, 4]) => [8, 2, 2, 2, 8]
+   example([2, 2, 2, 2, 2, 2]) => [4, 4, 4, 4, 4, 4]*/
+
+function DoubleValue() {
+  let numbers = [1, 2, 3, 4, 5];
+  return console.log(numbers.map(numbers => numbers * 2));
+ }
+ let result = DoubleValue();
+ console.log(result);
+
+/*2) Напишите функцию, которая заполняет новый массив предоставленным значением
+   и затем ее возвращает. Функция должна первым параметром принимать
+   элемент (значение для массива), вторым параметром размер массива.
+   Например: myFunc('a', 3)   // ['a', 'a', 'a']*/
+
+ function fillArray(element, size) {
+   let newArray = [];
+   for (let i = 0; i < size; i++) {
+     newArray.push(element);
+   }
+   return newArray;
+ }
+
+ console.log(fillArray('a', 3));
+
+
+ /*3) Напишите функцию, которая разворачивает массив в обратном порядке
+   и затем ее возвращает.  Функция наша  принимает массив любых элементов.
+   Например:
+   const data = [1, 2, 3];
+   myFunc(data);  // [3, 2, 1];*/
+
+ function revers(users = ['Max', 'Samir', 'Ulan']) {
+     let reversed = users.reverse();
+     console.log('reversed', reversed);
+ }
+
+ revers();

9cf9e5cdd9b1684dbfdfa6fd93624544947d7ad5
qqisa1 <ikramsabirov96@gmail.com>
2023-02-28 18:43:32 +0600

    feat: 26-02

--- 23-02/script.js
+++ 23-02/script.js
@@ -1,5 +1,5 @@
-const   hello = (mes1, mes2) =>{
-    alert(mes1, mes2);
+const   hello = (mes) =>{
+    alert(mes);
 }
-setTimeout(hello, 4000,'Hello 4');
-setTimeout(hello, 8000,'Hello 8');
+setTimeout(hello, 4000, 'Hello 4');
+setTimeout(hello, 8000, 'Hello 8');
